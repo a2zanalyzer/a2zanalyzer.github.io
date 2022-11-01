5e27fae9b5e773b172ae6be93646e18b96bd6102
a2zanalyzer <117021634+a2zanalyzer@users.noreply.github.com>
2022-11-01 17:31:54 +0530

    Update header.js

--- assets/analyzer/www.a2zbest.com/header.js
+++ assets/analyzer/www.a2zbest.com/header.js
@@ -1,1 +1,11 @@
-header.js
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B60PNEW7LY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B60PNEW7LY');
+</script>
